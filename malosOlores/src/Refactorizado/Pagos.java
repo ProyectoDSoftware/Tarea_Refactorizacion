a457927276c95932ed817bc199cd8cbfd99c7f28
Alvaro Valarezo <lfvalare@espol.edu.ec>
2019-01-10 22:44:14 -0500

    Arreglos metodos

--- malosOlores/src/Refactorizado/Pagos.java
+++ malosOlores/src/Refactorizado/Pagos.java
@@ -18,26 +18,28 @@ public class Pagos {
 
     //calcula el salario dependiendo del tipo de trabajador y entrega el décimo correspondiente cada 2 meses
     public float cs(Employee employee) {
-        Date date = new Date();
-        //Obtiene la hora local
-        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        //Obtiene el mes en forma de entero
-        int month = localDate.getMonthValue();
-        switch (employee.getEmployeeType()) {
-            case Worker:
-                //Si el mes es impar entonces le entrega el decimo junto con su salario
-                return Decimo(month, 0);
-            case Supervisor:
-                float valueS = employee.getSalary() + (employee.getBonusPercentage() * 0.5F);
-                //Si el mes es impar entonces le entrega el decimo junto con su salario y un bono
-                return Decimo(month, valueS);
-            case Manager:
-                float valueM = employee.getSalary()+ (employee.getBonusPercentage() * 0.7F);
-                //Si el mes es impar entonces le entrega el decimo junto con su salario y un bono
-                return Decimo(month, valueM);
-        }
-        return 0.0F;
-    }
+       Date date = new Date();
+       //Obtiene la hora local
+       LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+       //Obtiene el mes en forma de entero
+       int month = localDate.getMonthValue(); 
+       boolean isworker=employee.getEmployeeType().equals(EmployeeType.Worker);
+       boolean isSupervisor=employee.getEmployeeType().equals(EmployeeType.Supervisor);
+       boolean isManager=employee.getEmployeeType().equals(EmployeeType.Manager); 
+       if(isworker){
+            return Decimo(month, 0);
+       }else if(isSupervisor){
+            float valueS = employee.getSalary() + (employee.getBonusPercentage() * 0.5F);
+            //Si el mes es impar entonces le entrega el decimo junto con su salario y un bono
+            return Decimo(month, valueS);
+       }else if(isManager){
+            float valueM = employee.getSalary()+ (employee.getBonusPercentage() * 0.7F);
+            //Si el mes es impar entonces le entrega el decimo junto con su salario y un bono
+            return Decimo(month, valueM);
+       }
+       return 0.0F;
+    }    
+       
 
     public float Decimo(int month,float value){
         return month % 2 == 0 ? value : value + rmu / 12 * 2;
@@ -46,19 +48,20 @@ public class Pagos {
 
     //calcula el bonus anual
     public float CalculateYearBonus(Employee employee) {
-        switch (employee.getEmployeeType()) {
-            case Worker:
-                return 0;
-            case Supervisor:
-                return Bono(employee.getSalary(), 0.7F);
-            case Manager:
-                return Bono(employee.getSalary(), 1.0F);
-        }
-        return 0.0F;
-    }
-    
+       boolean isworker=employee.getEmployeeType().equals(EmployeeType.Worker);
+       boolean isSupervisor=employee.getEmployeeType().equals(EmployeeType.Supervisor);
+       boolean isManager=employee.getEmployeeType().equals(EmployeeType.Manager); 
+       if(isworker){
+            return 0;
+       }else if(isSupervisor){
+            return Bono(employee.getSalary(), 0.7F); 
+       }else if(isManager){
+            return Bono(employee.getSalary(), 1.0F);
+       }
+       return 0.0F;
+    }    
     public float Bono(float salary, float num){
         return salary + salary * num;
     }
-    
+ 
 }
